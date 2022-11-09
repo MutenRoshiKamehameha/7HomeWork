18e690aa07399e4dceced08af537a68a85bda2ad
baygeldi <akylbekuulubaygeldi@gmail.com>
2022-11-09 22:59:19 +0600

    ko

--- 7HomeWork/main.swift
+++ 7HomeWork/main.swift
@@ -9,3 +9,47 @@ import Foundation
 
 print("Hello, World!")
 
+//1 task
+
+extension Array{
+    func kvadrat< T: Numeric>(array:[T]){
+        for i in array {
+            
+            print(i * i)
+            
+        }
+    }
+}
+
+var array1 = [1,2,3,4,5,6,7]
+array1.kvadrat(array: array1)
+
+
+
+
+//2 task
+
+
+
+struct SearchItem <T: Equatable>{
+    
+    var mas: [T]
+    
+    func find(item: T)
+    {
+        
+        for i in mas {
+            
+            if i == item{
+                
+                print("элемент найден ")
+                break
+             }
+          }
+        print("элемент не найден ")
+     }
+}
+
+var o = SearchItem(mas: [2,4,4,23,5,2,4])
+
+o.find(item: 1)
